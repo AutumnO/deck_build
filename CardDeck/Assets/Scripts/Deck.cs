11f0c5b9928e370fdb5c54f51bd47d942fe2f504
Autumn Ossello <ao191@humboldt.edu>
2020-02-21 14:36:27 -0800

    2.21 Lab Added Buttons, need shuffle

---
+++ CardDeck/Assets/Scripts/Deck.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Deck
+{
+
+    public Button addToTopButton;
+    public Button removeFromTopButton;
+    public Button addToBottomButton;
+    public Button removeFromBottomButton;
+
+    #region Public methods
+
+    public void AddToTop(Card card)
+    {
+        Cards.AddLast(card);
+    }
+
+    public Card RemoveFromTop()
+    {
+        Card topCard = Cards.Last();
+        Cards.RemoveLast();
+        return topCard;
+
+    }
+
+    public void AddToBottom(Card card)
+    {
+        Cards.AddFirst(card);
+    }
+
+    public Card RemoveFromBottom()
+    {
+        Card bottomCard = Cards.First();
+        Cards.RemoveFirst();
+        return bottomCard;
+    }
+
+    #endregion
+
+
+
+    protected LinkedList<Card> Cards { get; set; }
+
+    public Deck()
+    {
+        Cards = new LinkedList<Card>();
+    }
+
+
+
+    public void Shuffle()
+    {
+         // private Vector2 card_vect = Cards.ToArray<Card>;
+         // need to add Adam's new code to all 
+    }
+
+}
